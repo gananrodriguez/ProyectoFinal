e3dba183acb60d50b084e561dd86cac75580f6c4
leo <leogrodriguez006@gmail.com>
2017-05-15 20:04:02 -0500

    loging

--- app/src/main/java/com/example/jhonatan/leonardo/MainActivity.java
+++ app/src/main/java/com/example/jhonatan/leonardo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.jhonatan.leonardo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button ingresar;
+
+
+
+        ingresar = (Button) findViewById(R.id.button);
+
+        ingresar.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View view) {
+
+        String usua = ((EditText) findViewById(R.id.editText)).getText().toString();
+        String contra = ((EditText) findViewById(R.id.editText2)).getText().toString();
+
+        if (usua.equals("lfgananrodriguezg@uniremingtonmanizales.edu.co") && contra.equals("leonardito")) {
+
+
+            Intent intent = new Intent(this,Main2Activity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    //
 }
